9668b9f8a4f36b852c7484431a58cf1c13862a4e
JamesWicks01 <115103781+JamesWicks01@users.noreply.github.com>
2024-01-25 00:50:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,20 +13,129 @@
 'use strict';
 
 function id() {
-  // e.g. return "UP654321";
+  return 2122177;
 }
 
 function fn() {
-  return 'Replace This With Your First Name';
+  return 'James';
 }
 
 function sn() {
-  return 'Replace This With Your Surname';
+  return 'Wicks';
 }
 
-function example() {
-  // replace this example with
-  // your first function then
-  // add more below as necessary.
+function add(a,b){
+  return a + b;
+}
 
+function subtract(a,b){
+  return a - b;
+}
+
+function checkObject(obj){
+  obj.checked = true;
+}
+
+function checkObjectInside(obj){
+  if(obj.data != null){
+    obj.data.checked = true;
+  }
+}
+
+function arraySet(arr, i , n){
+  if(arr[i]){
+    arr[i] = n;
+  }
+}
+
+function addAll(arr){
+  let total = 0;
+  for(let i = 0; i < arr.length; i++){
+    total = total + arr[i];
+  }
+  return total;
+}
+
+function larger(a,b){
+  return a >  b ? a : b;
+}
+
+function largest(arr){
+  let largest = arr[0] == null ? null : arr[0];
+
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > largest){
+      largest = arr[i];
+    }
+  }
+  return largest;
+}
+
+function compare(a,b){
+  if(a.length != b.length){
+    return false;
+  }
+
+  for(let i = 0; i < a.length; i++){
+    if(a[i] != b[i]){
+      return false
+    }
+  }
+  return true;
+}
+
+function addToAll(arr, n){
+  if(arr){
+    for(let i = 0; i < arr.length; i++){
+      arr[i] = arr[i] + n;
+    }
+  }
+  return arr;
+}
+
+let remembered;
+function rememberThis(keepsake){
+  remembered = keepsake; 
+}
+
+function nArray(n){
+  let array = [];
+
+  if(n){
+    for(let i = 0; i < n; i++){
+      array[i] = i + 1;
+    }
+    return array;
+  }
+}
+
+function addAllOpt(arr){
+  let sum = 0;
+  if(arr){
+    for(let i = 0; i < arr.length; i++){
+      sum =  sum + arr[i];
+    }
+  }
+  return sum;
+}
+
+function divisors(arr,div){
+  let newArr = []
+  for(let i = 0; i < arr.length; i++){
+    
+    if(arr[i] % div == 0){
+      newArr[newArr.length] = arr[i];
+    }
+  }
+  return newArr;
+}
+
+function multiples(n,m){
+  let array = []
+ 
+  for(let i = 0; i < n; i++){
+    array[i] = (i + 1) * m;
+  }
+
+  return array;
 }
